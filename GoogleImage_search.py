a5e40fe3827563b860de4f78cdeebdb009011774
Shivanshgpt13 <31778127+Shivanshgpt13@users.noreply.github.com>
2018-09-15 12:36:41 +0530

    Search Anything on Google Images

---
+++ GoogleImage_search.py
@@ -0,0 +1,42 @@
+#!usr/bin/pyhton
+# Importing Required Libraries
+
+import sys
+import time
+import webbrowser
+
+# Storing information into variable data through command line
+
+data= sys.argv[1: ]
+var_1= data[0]
+var_2= data[1]
+
+print ("please until two variable swap")
+
+time.sleep(1)
+# Swappiing Two Variables
+var_1,var_2= var_2,var_1
+
+
+print var_1
+print var_2
+
+# Concating two Strings
+result = var_1 + " " + var_2
+
+# google images will open in new tab on topic stored in variable result 
+webbrowser.open_new_tab('http://www.google.com/search?tbm=isch&q=%s'%result)
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
