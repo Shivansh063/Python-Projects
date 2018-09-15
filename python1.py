7ed441688541d78da3e906604a6b8802500dc292
Shivanshgpt13 <31778127+Shivanshgpt13@users.noreply.github.com>
2018-09-15 10:57:13 +0530

    Search Anything on Google
    Webbrowser module is used

--- python1.py
+++ python1.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env  python
 
-
+# Importing required Libraries
 import webbrowser
 import time
 import sys
 
-
+# Saving data from Command Line in to variable data
 data = sys.argv[1:3]
 
-n1 = data[0]
-n2 = data[1]
+var1= data[0]
+var2= data[1]
  
-n1,n2 = n2,n1
+# Swapping the values of two Variables
+var1,var2 = var2,var1
 print "Please wait until swapping is done" 
 time.sleep(2)
 
 
-print n1
-print n2
+print var1
+print var2
 
+# Concating two strings
 n3 = n1+" "+n2
 
+# After Concatination, Particular String will be on google new tab
 webbrowser.open_new_tab('http://google.com/search?btnG=1&q=%s'%n3)
 
 
