b13a930d1d5f29e6267157a61d0a680d07a7d97d
Shivanshgpt13 <31778127+Shivanshgpt13@users.noreply.github.com>
2018-09-15 13:19:11 +0530

    try _catch.py

---
+++ Error_detection.py
@@ -0,0 +1,36 @@
+#!usr/bin/env python
+
+import time
+import sys
+import math
+
+# Storing two values in variables value1, value2 through raw_input
+value1 = raw_input("Enter First Number :  ")
+value2 = raw_input("Enter Second Number :  ")
+
+'''
+try block for error generating statement 
+except block to catch error generated due to typcasting statements
+'''
+
+try:
+	n3 = int(n1) 
+	n4 = int(n2) 
+        sum = n3 + n4
+	print sum 
+except ValueError:
+	print ("    SYNTAX ERROR : cannot") 
+	
+
+
+
+
+
+
+
+
+
+	
+
+
+
