c68f88610f6749ad54be1787b9506fdcf0b33eaf
Shivanshgpt13 <31778127+Shivanshgpt13@users.noreply.github.com>
2018-09-16 10:57:51 +0530

    Looping

---
+++ for_task.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python 
+
+
+import sys
+import time
+
+# Initialising two variables accordingly
+sum = 0
+mul = 1
+
+
+# Looping Addition
+for i in range(5) :
+	sum= sum + (i+1)
+
+
+print ("Total sum : "),sum
+
+# Looping Multipication
+for i in range(5) :
+	mul = mul * (i+1)
+
+print ("Multiplication is : "), mul
