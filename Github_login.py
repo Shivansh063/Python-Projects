64fc38145542b08ad901a0f230abeade826d6ccf
Shivanshgpt13 <31778127+Shivanshgpt13@users.noreply.github.com>
2018-12-29 13:26:47 +0530

    GitHub Login Through Selenium

---
+++ Github_login.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+# importing libraries 
+
+from selenium import webdriver 
+from time import sleep 
+from getpass import getpass
+ 
+# Storing E-mail and Password in variable usr and pwd
+ 
+usr=input('Enter Email Id:')  
+pwd=getpass('Enter Password:')  
+  
+# Using Firefox 
+driver = webdriver.Firefox() 
+driver.get("https://github.com/login") 
+print ("Opened facebook") 
+sleep(1) 
+  
+#Finding element through id 
+username_box = driver.find_element_by_id('login_field') 
+username_box.send_keys(usr) 
+print ("Email Id entered") 
+sleep(1) 
+  
+password_box = driver.find_element_by_id('password') 
+password_box.send_keys(pwd) 
+print ("Password entered") 
+  
+login_box = driver.find_element_by_name('commit') 
+login_box.click() 
+  
+print ("Done") 
+input('Press anything to quit') 
+driver.quit() 
+print("Finished") 
+
+
+'''
+
+from getpass import getpass 
+pwd = getpass('Enter Password:')  
+'''
